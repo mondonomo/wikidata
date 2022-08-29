44661a8466cd2ebb7820db3241ca187b9870fdc5
dlauc <davor@lauc.org>
2022-08-30 01:40:02 +0200

    wiki lang

--- text_utils.py
+++ text_utils.py
@@ -2,8 +2,13 @@ import re
 
 zag = re.compile(' ?\([^\)]*\)')
 resplit = re.compile('/|\n')
+retok = re.compile("[\u200B\u202F\u205F\u3000\s・]+")
 
 
 def cl(s):
     c = zag.sub('', s).strip()
     return c
+
+
+if __name__ == '__main__':
+    print(retok.split('ジョージ・ワシントン'))
