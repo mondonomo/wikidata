4d026c1fbd2a74e46cb4ac6727f7a7d8d8d48a7d
dlauc <davor@lauc.org>
2024-03-04 20:31:52 +0000

    wiki frequency dictionary

--- create_freq_dict.py
+++ create_freq_dict.py
@@ -9,6 +9,7 @@ sys.path.insert(0, '../nelma')
 sys.path.insert(0, '../nelma/mondoDB')
 
 from mondoDB.mtokenize import m_tokenize
+from mondoDB.get_script import get_script
 from wiki_labels import qid_lab_get, qid_lab
 
 
@@ -28,21 +29,26 @@ if __name__ == '__main__':
     (named_ent, types_c) = pickle.load(open('wikinelma_ids.pickle', 'rb'))
 
     if True:
+        START = 30_000_000
         dic = defaultdict(Counter)
 
         maxq = qid_lab.shape[0]
         # maxq = 1000
-        for qid in tqdm(range(1, maxq), total=maxq):
+        for qid in tqdm(range(START, maxq), total=maxq-START):
             ent_t = named_ent[qid] if qid in named_ent else 'O'
             for label, langs in qid_lab_get(qid, return_alt=True).items():
-                for lang, _ in langs:
-                    if lang[2] == '_' and len(lang)>=7:
-                        lang2, script = lang[:7].split('_')
-                        for token in m_tokenize(label, lang2, script, True):
-                            dic[f'{lang2}_{script}_{token}'][ent_t] += 1
+                lang2 = None
+                if len(lang) >= 7 and lang[2] == '_':
+                    lang2 = lang[:2].split('_')
+                if len(lang) == 2:
+                    lang2 = lang
+                if lang:
+                    script = get_script(label)
+                    for token in m_tokenize(label, lang2, script, True):
+                        dic[f'{lang2}_{script}_{token}'][ent_t] += 1
             if qid % 1_000_000 == 0:
-                pickle.dump(dic, open('wiki_freq_dict.pickle', 'wb'))
+                pickle.dump(dic, open('wiki_freq_dict_from60M.pickle', 'wb'))
                 
-        pickle.dump(dic, open('wiki_freq_dict.pickle', 'wb'))
+        pickle.dump(dic, open('wiki_freq_from60M_dict.pickle', 'wb'))
 
 
